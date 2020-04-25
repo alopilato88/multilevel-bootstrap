3cd896292c48959e5212f80a002c959c73148ad4
Alex LoPilato <Alex_LoPilato@mckinsey.com>
2020-04-25 17:57:36 -0400

    Initial commit to create the simMLMMediation.R script.

---
+++ scripts/simMLMMediation.R
@@ -0,0 +1,4 @@
+#######################################################
+# This script contains the code needed to create the  #
+# simMLMMediation function.                           #  
+#######################################################
